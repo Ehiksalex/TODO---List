8f9088e87aff053ef661a9d22bbc54c11cfbf09a
Ehiksalex <ehiksalex@gmail.com>
2022-05-27 01:03:51 +0100

    Updated

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ clearList();
     
 //adding a new list and creating error message
 var error = document.querySelector('.error')
-var myUl = document.getElementsByClassName('list');
+var myUl = document.getElementById('todos');
 var btn = document.querySelector(".btn");
 var inputItem = document.querySelector(".form-control");
+var li = document.querySelector('li')
     function newList(){
         if(inputItem.value == 0){
             error.innerHTML = "Input a list";
@@ -28,15 +29,25 @@ var inputItem = document.querySelector(".form-control");
             var li = document.createElement("li");
             var txtNode = document.createTextNode(inputItem.value);
             li.appendChild(txtNode);
-            myUl.appendChild(li)
+            li.classList.add('list-group-item');
+            var i = document.createElement(i);
+            i.classList.add('delete');
+            i.classList.add('fa');
+            i.classList.add('fa-trash-o');
+            li.appendChild(i);
+            myUl.appendChild(li);
             error.innerHTML = "";
             inputItem.value = "";
-    
+        clearList();
+            
     
         }
-        clearList();
+        
         
     }
+
+
+    
     //make the add button execute the addition of new list
     
     btn.addEventListener('click', newList);
